b1561589f54e43604990740b679cb120cfeec947
Jun20220703 <kkjhhyu0405@gmail.com>
2024-07-11 13:48:37 +0800

    Update register_email.php

--- UserProfile/register_email.php
+++ UserProfile/register_email.php
@@ -14,13 +14,13 @@ function sendWelcomeEmail($email, $username) {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com'; // Gmail SMTP 서버 주소
         $mail->SMTPAuth = true;
-        $mail->Username = 'your-email@gmail.com'; // 실제 사용할 Gmail 주소
-        $mail->Password = 'your-email-password'; // Gmail 비밀번호
+        $mail->Username = 'kkjhhyu0405@gmail.com'; // 실제 사용할 Gmail 주소
+        $mail->Password = 'kjh030407@'; // Gmail 비밀번호
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
         // 수신자 설정
-        $mail->setFrom('your-email@gmail.com', 'Your Name'); // 실제 사용할 Gmail 주소와 발신자 이름
+        $mail->setFrom('kkjhhyu0405@gmail.com', '김준형'); // 실제 사용할 Gmail 주소와 발신자 이름
         $mail->addAddress($email, $username);
 
         // 이메일 내용 설정
