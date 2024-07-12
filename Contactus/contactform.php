327cd440f8f49784f1a844ba73c5b3dbd1adb9df
MrQin877 <kuanchinzhong@gmail.com>
2024-07-13 03:28:10 +0800

    haizz

---
+++ Contactus/contactform.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $phnum = $_POST['phnum'];
+    $question = $_POST['question'];
+    $program = $_POST['program'];
+
+    $sql = "INSERT INTO contact_form (fullname, email, phoneno, question, program) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sssss", $name, $email, $phnum, $question, $program);
+
+    if ($stmt->execute()) {
+        echo "<script>alert('Changes saved successfully'); window.location.href = 'ContactUS.html';</script>";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+}
+?>
