0d4f24cd805cde363ffca1c483ef4d2179972a66
Jun20220703 <kkjhhyu0405@gmail.com>
2024-07-13 23:16:52 +0800

    Update send_reset_email.php

--- LogRes_sys/send_reset_email.php
+++ LogRes_sys/send_reset_email.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require __DIR__ . '/vendor/autoload.php'; // Adjust the path if necessary
+
 
 $servername = "localhost";
 $username = "root";
