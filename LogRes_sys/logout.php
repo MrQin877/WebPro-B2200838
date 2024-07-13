f4b45c91e3654df662cf493ac40d99c65463d0dc
Jun20220703 <kkjhhyu0405@gmail.com>
2024-07-13 13:15:46 +0800

    ok

--- LogRes_sys/logout.php
+++ LogRes_sys/logout.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
-session_destroy(); // 모든 세션 데이터 제거
-header('Location: index.php'); // 로그아웃 후 리디렉션
+
+$_SERVER= array();
+
+session_destroy();
+
+header("Location: A-HomePage/HomePage.html");
 exit();
 ?>
