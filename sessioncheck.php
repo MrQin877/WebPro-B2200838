d8217801f3aff8b3aee0a4dfaf96a68e1d120de1
Jun20220703 <kkjhhyu0405@gmail.com>
2024-07-13 13:25:16 +0800

    Update sessioncheck.php

--- sessioncheck.php
+++ sessioncheck.php
@@ -4,15 +4,22 @@ header('Content-Type: application/json');
 
 $response = [];
 
-if (isset($_SESSION['user_id'])) {
+// Function to check if user is logged in
+function check_login() {
+    return isset($_SESSION['user_id']);
+}
+
+// Initialize response variables
+if (check_login()) {
     $response['loggedIn'] = true;
     $response['user_id'] = $_SESSION['user_id'];
-    $response['isAdmin'] = $_SESSION['user_id'] == 0;
+    $response['isAdmin'] = ($_SESSION['user_id'] == 0); // Assuming admin user ID is 0
 } else {
     $response['loggedIn'] = false;
     $response['isAdmin'] = false;
 }
 // password:smartstudy1507@sport gmail: admin1507@smartstudysport.com
+
 echo json_encode($response);
 ?>
 
