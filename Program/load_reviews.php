96c997fe655ff63382edc7f125a067f2bc95ce67
Jun20220703 <kkjhhyu0405@gmail.com>
2024-07-13 19:19:43 +0800

    Update load_reviews.php

--- Program/load_reviews.php
+++ Program/load_reviews.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
 }
 
 $program = $_GET['program'];
-$stmt = $conn->prepare("SELECT review, star, email FROM user_review WHERE program = ?");
+$stmt = $conn->prepare("SELECT review, star, program FROM user_review WHERE program = ?");
 $stmt->bind_param("s", $program);
 $stmt->execute();
 $result = $stmt->get_result();
