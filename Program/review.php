69a51ad4176057d5e30e09bb462722b55df56f70
Jun20220703 <kkjhhyu0405@gmail.com>
2024-07-13 11:55:10 +0800

    Update review.php

--- Program/review.php
+++ Program/review.php
@@ -16,9 +16,10 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $review = $_POST['review'];
     $star = $_POST['star'];
+    $program = $_POST['program'];
     
 
-    $sql = "INSERT INTO user_review (review, star) VALUES (?, ?, ?)";
+    $sql = "INSERT INTO user_review (review, star, program) VALUES (?, ?, ?, ?)";
 
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("sss", $review, $star);
