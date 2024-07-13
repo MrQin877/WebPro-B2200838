0059590d5e21b67541cdd9e8074cf08816003fe9
Jun20220703 <kkjhhyu0405@gmail.com>
2024-07-13 18:44:30 +0800

    ok

---
+++ Program/load_reviews.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user"; // 데이터베이스 이름으로 변경하세요
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$program = $_GET['program'];
+$stmt = $conn->prepare("SELECT review, star, program FROM user_review WHERE program = ?");
+$stmt->bind_param("s", $program);
+$stmt->execute();
+$result = $stmt->get_result();
+
+$reviews = [];
+while ($row = $result->fetch_assoc()) {
+    $reviews[] = $row;
+}
+
+$stmt->close();
+$conn->close();
+
+header('Content-Type: application/json');
+echo json_encode(['reviews' => $reviews]);
+?>
