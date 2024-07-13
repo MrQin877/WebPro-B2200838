f6084bab7fe7647d9be55c50c2410e3f46f819f5
zc100504 <122176250+zc100504@users.noreply.github.com>
2024-07-13 16:31:47 +0800

    gg

--- LogRes_sys/check_session.php
+++ LogRes_sys/check_session.php
@@ -6,6 +6,7 @@ $response = [];
 
 if (isset($_SESSION['user_id'])) {
     $response['loggedIn'] = true;
+    $response['user_id'] = $_SESSION['user_id'];
 } else {
     $response['loggedIn'] = false;
 }
