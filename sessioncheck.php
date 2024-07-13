785840af663e424eac422e64b6c6c77c713c9abf
Jun20220703 <kkjhhyu0405@gmail.com>
2024-07-13 13:51:17 +0800

    ok

--- sessioncheck.php
+++ sessioncheck.php
@@ -7,11 +7,11 @@ $response = [];
 if (isset($_SESSION['user_id'])) {
     $response['loggedIn'] = true;
     $response['user_id'] = $_SESSION['user_id'];
-    $response['isAdmin'] = ($_SESSION['user_id'] == 0); // isAdmin 조건 수정
+    $response['isAdmin'] = $_SESSION['user_id'] == 0;
 } else {
     $response['loggedIn'] = false;
     $response['isAdmin'] = false;
 }
-
+// password:smartstudy1507@sport gmail: admin1507@smartstudysport.com
 echo json_encode($response);
 ?>
