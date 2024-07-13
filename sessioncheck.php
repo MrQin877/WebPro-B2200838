f78062cf24e819408851467733ef9e48161eff3d
Jun20220703 <kkjhhyu0405@gmail.com>
2024-07-13 13:47:46 +0800

    ok

--- sessioncheck.php
+++ sessioncheck.php
@@ -8,12 +8,12 @@ if (isset($_SESSION['user_id'])) {
     $response['loggedIn'] = true;
     $response['user_id'] = $_SESSION['user_id'];
     $response['isAdmin'] = ($_SESSION['user_id'] == 0); // isAdmin 조건 수정
+    $response['isLoggedIn'] = true; // 로그인 상태이므로 isLoggedIn을 true로 설정
 } else {
     $response['loggedIn'] = false;
     $response['isAdmin'] = false;
+    $response['isLoggedIn'] = false; // 로그인 상태가 아니므로 isLoggedIn을 false로 설정
 }
-
 // password:smartstudy1507@sport gmail: admin1507@smartstudysport.com
-
 echo json_encode($response);
 ?>
